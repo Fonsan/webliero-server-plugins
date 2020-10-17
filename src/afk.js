5ede998b842c0a4a2d944c7a8511765398ee2a79
Fonsan <fonsan@gmail.com>
2020-10-17 02:17:50 +0200

    Initial

---
+++ src/afk.js
@@ -0,0 +1,62 @@
+(function () {
+  const chainFunction = (object, attribute, func) => {
+    const original = object[attribute]
+    if (original) {
+      object[attribute] = (...arguments) => {
+        original.apply(object, arguments)
+        func.apply(object, arguments)
+      }
+    } else {
+      object[attribute] = func
+    }
+  }
+  return new Promise((resolve, reject) => {
+    const room = window.WLROOM
+    if (room.AFK_PLUGIN) {
+      throw 'afk already loaded'
+    }
+    const settings = {
+      timeout: 3000,
+      graceTime: 1000
+    }
+    room.AFK_PLUGIN = true
+    chainFunction(room, 'onPlayerJoin', (player) => {
+      const motd = `AFK detection loaded, players are kicked after ${settings.timeout / 1000} seconds of inactivity`
+      room.sendAnnouncement(motd, player.id)
+    })
+    const playingPlayers = {}
+    const evictPlayer = (playerId) => {
+      const message = `You will be evicted due too inactivity in ${settings.graceTime / 1000}, please move`
+      room.sendAnnouncement(message, playerId)
+      const currentTimeout = playingPlayers[playerId]
+      setTimeout(() => {
+        if (playingPlayers[playerId] == currentTimeout) {
+          const reason = `You were afk for more than ${settings.timeout / 1000} seconds`
+          room.kickPlayer(playerId, 'afk')
+        }
+      }, settings.graceTime)
+    }
+    const clearPlayerTimeout = (playerId) => {
+      if(playingPlayers[playerId]) {
+        clearTimeout(playingPlayers[playerId])
+      }
+      delete playingPlayers[playerId]
+    }
+    const resetPlayerTimeout = (playerId) => {
+      clearPlayerTimeout(playerId)
+      playingPlayers[playerId] = setTimeout(evictPlayer.bind(null, playerId), settings.timeout)
+    }
+
+    chainFunction(room, 'onPlayerTeamChange', (player) => {
+      if (player.team == 0) {
+        clearPlayerTimeout(player.id)
+      } else {
+        resetPlayerTimeout(player.id)
+      }
+    })
+
+    chainFunction(room, 'onPlayerActivity', (player) => {
+      resetPlayerTimeout(player.id)
+    })
+  })
+})()
